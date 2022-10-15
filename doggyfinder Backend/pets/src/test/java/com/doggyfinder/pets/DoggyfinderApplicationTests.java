acad0808b8be91d40404e33c7fd2a6a3876a2b19
s3rodriguez10 <s3rodriguez10@gmail.com>
2022-10-15 18:54:51 -0500

    Add Spring boot Project Pets

---
+++ doggyfinder Backend/pets/src/test/java/com/doggyfinder/pets/DoggyfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doggyfinder.pets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggyfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
